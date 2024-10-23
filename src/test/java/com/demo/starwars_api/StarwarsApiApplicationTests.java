12b945b263008adf80cbfe4d63a88ba431414ef3
siminio <14363367+siminio@users.noreply.github.com>
2024-10-23 21:35:03 +0200

    Initializate project

---
+++ src/test/java/com/demo/starwars_api/StarwarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.starwars_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
